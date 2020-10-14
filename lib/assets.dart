8d5b385c56761ad6ec1c527a7cc3563f122aa39b
chanhui <chyaohui@gmail.com>
2020-10-14 19:20:06 +0800

    Create assets.dart

---
+++ lib/assets.dart
@@ -0,0 +1,7 @@
+class Assets {
+  static const String _images = 'assets/images/';
+  static const String _audios = 'assets/mp3/';
+  static const String _fonts = 'assets/fonts/';
+  static const String cherry = '${_images}cherry.svg';
+
+}
